d61ce4b023f4e0823ca061cf8684af43bb3d1323
Santhosh Raaj K R <santhoshraajkr.17@gmail.com>
2025-06-22 16:50:18 +0530

    updated with upload page

---
+++ src/components/Upload.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Upload = () => {
+  return (
+    <div className="w-screen h-screen bg-[#0E1010] relative overflow-hidden">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="w-[850px] h-[850px] bg-[radial-gradient(circle,_#175553_0%,_transparent_60%)] rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
